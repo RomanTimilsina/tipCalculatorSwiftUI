cdee3df7259189a96294cd3a8a8e946f550c2bb2
Roman <romantimilsina@gmail.com>
2023-11-20 15:06:22 +0545

    finished

--- TipCalculatorSwiftUI/ContentView.swift
+++ TipCalculatorSwiftUI/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let tipValue = checkAmount / 100 * Double(tipPercentage)
+        let totalAmount = checkAmount + tipValue
+        let tipPerPerson = totalAmount / Double(numberOfPeople + 2)
+
+        return tipPerPerson
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        //Finished
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code:Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number Of People", selection: $numberOfPeople){
+                        ForEach(2..<100) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("How much do you want to tip?") {
+                    Picker("Tip percentages", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section{
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("Tip Calculator")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
