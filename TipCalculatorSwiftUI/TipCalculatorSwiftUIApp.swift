9a3f54eaa6f36b22d633ec355b00556098135299
Roman <romantimilsina@gmail.com>
2023-11-20 11:34:02 +0545

    Initial Commit

---
+++ TipCalculatorSwiftUI/TipCalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorSwiftUIApp.swift
+//  TipCalculatorSwiftUI
+//
+//  Created by Mac on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
